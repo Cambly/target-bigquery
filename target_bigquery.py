11de515d0268f2397f8e85252d1f9b2e386abfcf
Charlie Axelrod <caxelrod@Charlies-MBP.localdomain>
2019-06-27 14:54:54 -0700

    Don't print number of rows loaded

--- target_bigquery.py
+++ target_bigquery.py
@@ -269,11 +269,11 @@ def persist_lines_stream(project_id, dataset_id, lines=None, validate_records=Tr
         else:
             raise Exception("Unrecognized message {}".format(msg))
 
-    for table in errors.keys():
-        if not errors[table]:
-            print('Loaded {} row(s) into {}:{}'.format(rows[table], dataset_id, table), tables[table].path)
-        else:
-            print('Errors:', errors[table], sep=" ")
+    # for table in errors.keys():
+    #     if not errors[table]:
+    #         print('Loaded {} row(s) into {}:{}'.format(rows[table], dataset_id, table), tables[table].path)
+    #     else:
+    #         print('Errors:', errors[table], sep=" ")
 
     return state
 
